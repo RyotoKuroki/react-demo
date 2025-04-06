60d97f27d52a14fa4d5f6f5c55e986acb26b3f76
RyotoKuroki <kurohoge@gmail.com>
2025-04-06 06:45:56 +0000

    a

--- src/funcs/FetchDemo.ts
+++ src/funcs/FetchDemo.ts
@@ -1,20 +1,13 @@
 import axios from "axios";
+import env from "../env/env";
 
 export default class FetchDemo {
-
 
     public async FetchHelloWorld (
         callback: (data: any) => void
     ) {
         try {
-
-            // ローカルサーバ接続確認 OK
-            const url1 = "http://localhost:1337/api/t-hoges";
-            // Lambda接続確認（テンプレートでCors設定していないため不可）
-            const url2 = "https://gm26dtewgesnis7g3g3vlebeim0pcdyxx.lambda-url.ap-southeast-1.on.aws/";
-            // Lambda接続確認（テンプレートでCors設定しているため可）
-            const url3 = "https://c76nh5rn6yslmeentdsoqk3gsy0btqqtt.lambda-url.ap-southeast-1.on.aws/";
-            const response = await axios.get(url3, {
+            const response = await axios.get(env.URL__LAMBDA_TEST_2, {
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json',
